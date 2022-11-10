69315b1204c26dc5974c68012a5e079d0ac416c2
tomasreyna <117814821+tomasreyna@users.noreply.github.com>
2022-11-10 10:12:00 +0100

    Jenkinsfile
    Jenkinsfile

--- src/test/java/myjenktest/FizzBuzzTest.java
+++ src/test/java/myjenktest/FizzBuzzTest.java
@@ -1,5 +1,25 @@
 package myjenktest;
 
-public class FizzBuzzTest {
+import org.junit.jupiter.api.*;
 
+
+import myjenktest.tutorial.FizzBuzz;
+
+public class FizzBuzzTest {
+	public FizzBuzz fb;
+	
+	@BeforeEach 
+	public void setup()	{
+		fb=new FizzBuzz();
+		
+	}
+	
+	@DisplayName("Play FizzBuzz with number=1")
+	@Test
+	public void testNumber() {
+		String risultato=fb.play(1);
+		//assertEquals(risultatoOttenuto, risultatoPianificato)
+		Assertions.assertEquals(risultato, "1" );
+		
+	}
 }
